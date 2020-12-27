d066121259b7497bf176438fc1934500f19b35cf
Marina Osborne <65596875+MarinaOsborne@users.noreply.github.com>
2020-12-27 21:18:57 +0300

    Update RegisterForm.js

--- public/js/ui/forms/RegisterForm.js
+++ public/js/ui/forms/RegisterForm.js
@@ -3,13 +3,29 @@
  * регистрации
  * Наследуется от AsyncForm
  * */
-class RegisterForm {
+class RegisterForm extends AsyncForm {
   /**
    * Производит регистрацию с помощью User.register
    * После успешной регистрации устанавливает
    * состояние App.setState( 'user-logged' )
    * и закрывает окно, в котором находится форма
    * */
+
   onSubmit( options ) {
+
+    let element = this.element;
+    User.register(options, callback);
+
+    function callback(err, response) {
+
+      if (response.success == true) {
+        let elementForClose = element.closest("#modal-register");
+
+        App.setState("user-logged");
+        new Modal(elementForClose).close();
+      } else {
+        console.log("ошибка: " + err);
+      }
+    }
   }
 }
